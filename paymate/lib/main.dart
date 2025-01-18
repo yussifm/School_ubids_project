f97feb422d44d3379f86775f420c40d555018562
Mohammed Yussif <yussifm13@gmail.com>
2025-01-18 18:56:51 +0000

    .

--- paymate/lib/main.dart
+++ paymate/lib/main.dart
@@ -38,6 +38,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return  OnboardingScreen();
+    return const OnboardingScreen();
   }
 }
